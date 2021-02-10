28c59abd23f9fcaac810ef3809ce84f88fac9b79
Anthony <mosso135@gmail.com>
2021-02-10 18:47:47 +0100

    Database Debugging

--- server/database/index.js
+++ server/database/index.js
@@ -11,41 +11,41 @@ const CONNECTION_STRING = new Pool({
     }
 });
 
-class Database {
-    constructor() {
-        this._pool = new Pool({
-            connectionString: CONNECTION_STRING,
-            ssl: SSL
+// class Database {
+//     constructor() {
+//         this._pool = new Pool({
+//             connectionString: CONNECTION_STRING,
+//             ssl: SSL
+//         });
+
+//         this._pool.on('error', (err, client) => {
+//             console.error('Unexpected error on idle PostgreSQL client.', err);
+//             process.exit(-1);
+//         });
+
+//     }
+
+query(query, ...args) {
+    this._pool.connect((err, client, done) => {
+        if (err) throw err;
+        const params = args.length === 2 ? args[0] : [];
+        const callback = args.length === 1 ? args[0] : args[1];
+
+        client.query(query, params, (err, res) => {
+            done();
+            if (err) {
+                console.log(err.stack);
+                return callback({ error: 'Database error.' }, null);
+            }
+            callback({}, res.rows);
         });
+    });
 
-        this._pool.on('error', (err, client) => {
-            console.error('Unexpected error on idle PostgreSQL client.', err);
-            process.exit(-1);
-        });
+}
 
-    }
-
-    query(query, ...args) {
-        this._pool.connect((err, client, done) => {
-            if (err) throw err;
-            const params = args.length === 2 ? args[0] : [];
-            const callback = args.length === 1 ? args[0] : args[1];
-
-            client.query(query, params, (err, res) => {
-                done();
-                if (err) {
-                    console.log(err.stack);
-                    return callback({ error: 'Database error.' }, null);
-                }
-                callback({}, res.rows);
-            });
-        });
-
-    }
-
-    end() {
-        this._pool.end();
-    }
+end() {
+    this._pool.end();
+}
 }
 
 module.exports = new Database();
