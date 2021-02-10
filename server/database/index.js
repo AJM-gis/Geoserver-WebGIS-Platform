09744fdeab225442f505520492325398259581c6
Anthony <mosso135@gmail.com>
2021-02-10 18:52:56 +0100

    Database Debugging

--- server/database/index.js
+++ server/database/index.js
@@ -1,51 +1,45 @@
 var { Pool } = require('pg');
 
-// const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/weather-db-ajm';
-// const SSL = process.env.NODE.ENV === 'production';
+const CONNECTION_STRING = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/weather-db-ajm';
+const SSL = process.env.NODE.ENV === 'production';
 
-
-const CONNECTION_STRING = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-// class Database {
-//     constructor() {
-//         this._pool = new Pool({
-//             connectionString: CONNECTION_STRING,
-//             ssl: SSL
-//         });
-
-//         this._pool.on('error', (err, client) => {
-//             console.error('Unexpected error on idle PostgreSQL client.', err);
-//             process.exit(-1);
-//         });
-
-//     }
-
-query(query, ...args) {
-    this._pool.connect((err, client, done) => {
-        if (err) throw err;
-        const params = args.length === 2 ? args[0] : [];
-        const callback = args.length === 1 ? args[0] : args[1];
-
-        client.query(query, params, (err, res) => {
-            done();
-            if (err) {
-                console.log(err.stack);
-                return callback({ error: 'Database error.' }, null);
+class Database {
+    constructor() {
+        this._pool = new Pool({
+            connectionString: CONNECTION_STRING,
+            ssl: {
+                rejectUnauthorized: false
             }
-            callback({}, res.rows);
         });
-    });
 
-}
+        this._pool.on('error', (err, client) => {
+            console.error('Unexpected error on idle PostgreSQL client.', err);
+            process.exit(-1);
+        });
 
-end() {
-    this._pool.end();
-}
+    }
+
+    query(query, ...args) {
+        this._pool.connect((err, client, done) => {
+            if (err) throw err;
+            const params = args.length === 2 ? args[0] : [];
+            const callback = args.length === 1 ? args[0] : args[1];
+
+            client.query(query, params, (err, res) => {
+                done();
+                if (err) {
+                    console.log(err.stack);
+                    return callback({ error: 'Database error.' }, null);
+                }
+                callback({}, res.rows);
+            });
+        });
+
+    }
+
+    end() {
+        this._pool.end();
+    }
 }
 
 module.exports = new Database();
