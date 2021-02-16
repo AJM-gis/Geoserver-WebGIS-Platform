c4b423db8e9f90fea42060e5629a52e76e2217b7
Anthony <mosso135@gmail.com>
2021-02-16 10:34:36 +0100

    Updating API Info

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,11 +1,11 @@
 const request = require('request-promise');
 
-const API_KEY = '881a5f65e22a9ee0159681336d9eafe9';
+require('dotenv').config();
 
 class Weather {
     static retrieveByCity(city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=metric`,
             json: true
         }).then(function (res) {
             callback(res);
